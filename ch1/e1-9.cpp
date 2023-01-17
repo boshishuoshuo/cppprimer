d800263acbb49697b0f1439e620639b73c7634f9
Yan Feng <fengyan0121@gmail.com>
2023-01-17 13:07:20 -0500

    ex ch1 1/17/23

---
+++ ch1/e1-9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    int i = 50, sum = 0;
+    while (i <= 100) {
+        sum += i;
+        i++;
+    }
+    std::cout << "sum is " << sum << std::endl;
+    return 0;
+}
